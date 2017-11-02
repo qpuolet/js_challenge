88831fbc7376e7a22d5de4ea6675718e61c484ed
qpuolet <qpuolet@gmail.com>
2017-11-03 01:27:10 +0300

    Update: done

--- task_1/sumOfTwo.js
+++ task_1/sumOfTwo.js
@@ -1,3 +1,19 @@
 function sumOfTwo(array1, array2, x) {
+    let res = array1.map(a1 => array2.map(a2 => a1+a2))
+            .map(f1 => f1.filter(n => n === x))
+                .filter(l => l.length>=1).length;
+    return res > 0;
+}
 
+
+function sumOfTwo2(arr1, arr2, x){
+
+    for(let i = 0; i < arr1.length; i++){
+        for(let j = 0; j < arr2.length; j++){
+            if(arr1[i]+arr2[j] === x) {
+                return true
+            }
+        }
+    }
+    return false;
 }
--- task_2/houseRobber.js
+++ task_2/houseRobber.js
@@ -1,3 +1,13 @@
 function houseRobber(nums) {
-
+    if(nums.length === 0){
+        return nums[0];
+    } else if(nums.length === 1) {
+        return Math.max(nums[0], nums[1])
+    } else {
+        let res = [nums[0], Math.max(nums[0], nums[1])]
+        for(let i = 2; i < nums.length; i++){
+            res.push(Math.max(nums[i] + res[i-2], res[i-1]));
+        }
+        return res[res.length-1];
+    }
 }
